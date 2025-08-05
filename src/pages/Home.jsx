6ee6a19c57fc45faa3fa8333c94291ac3653aa91
RJ <raven.your.dream@gmail.com>
2025-08-05 19:20:11 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { MailIcon, PhoneIcon } from '@heroicons/react/24/outline';
+import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline';
 
 export default function Home() {
   return (

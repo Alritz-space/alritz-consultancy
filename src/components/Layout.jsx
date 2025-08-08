6dbdf378bc8da7e6c47c3eeb366176b75adc23cd
RJ <raven.your.dream@gmail.com>
2025-08-08 21:45:04 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 // src/components/Layout.jsx
 import React from 'react';
 import { Outlet, Link, useLocation } from 'react-router-dom';
-import { FaFacebookF, FaXTwitter, FaLinkedinIn, FaWhatsapp } from 'react-icons/fa';
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaWhatsapp } from 'react-icons/fa';
 
 export default function Layout() {
   const location = useLocation();

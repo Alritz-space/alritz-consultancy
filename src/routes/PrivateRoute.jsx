e504bacab5121ddc63986c2b9a9c61a7d13b87bb
RJ <raven.your.dream@gmail.com>
2025-08-08 23:18:52 +0530

    Update PrivateRoute.jsx

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 
-export default function PrivateRoute({ authenticated }) {
-  return authenticated ? <Outlet /> : <Navigate to="/login" />;
+export default function PrivateRoute({ component: Component, ...rest }) {
+  const authenticated = true;
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authenticated ? <Component {...props} /> : <Redirect to={'/login'} />
+      }
+    />
+  );
 }
